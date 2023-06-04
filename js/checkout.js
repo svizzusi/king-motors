2580ef9565cea677200d65ee22987f495f735aee
Stephen Vizzusi <svizzusi13@gmail.com>
2023-06-04 14:01:02 -0700

    Need to fix the email JS

--- js/checkout.js
+++ js/checkout.js
@@ -10,30 +10,31 @@ closeButton.addEventListener('click', () => {
     location.reload()
 })
 
-// checkOutForm.addEventListener('submit',(event) => {
-//     event.preventDefault();
-//     // netlify website form handler
-//     submitContainer.style.display = "flex";
-//     checkOutForm.reset();
-// })
+checkOutForm.addEventListener('submit',(event) => {
+    event.preventDefault();
+    // netlify website form handler
+    submitContainer.style.display = "flex";
+    checkOutForm.reset();
+})
 
-(function() {
-    // https://dashboard.emailjs.com/admin/account
-    emailjs.init('If7t9Q2qS_tvKYRZR');
-})();
+// (function() {
+//     // https://dashboard.emailjs.com/admin/account
+//     emailjs.init('If7t9Q2qS_tvKYRZR');
+//     alert('message')
+// })();
 
-window.onload = function() {
-    checkOutForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        // generate a five digit number for the contact_number variable
-        this.contact_number.value = Math.random() * 100000 | 0;
-        // these IDs from the previous steps
-        emailjs.sendForm('service_iznybsu', 'template_vajrc3o', this)
-            .then(function() {
-                submitContainer.style.display = "flex";
-                checkOutForm.reset();
-            }, function(error) {
-                alert('FAILED...', error);
-            });
-    });
-}
+// window.onload = function() {
+//     checkOutForm.addEventListener('submit', function(event) {
+//         event.preventDefault();
+//         // generate a five digit number for the contact_number variable
+//         this.contact_number.value = Math.random() * 100000 | 0;
+//         // these IDs from the previous steps
+//         emailjs.sendForm('service_iznybsu', 'template_vajrc3o', this)
+//             .then(function() {
+//                 submitContainer.style.display = "flex";
+//                 checkOutForm.reset();
+//             }, function(error) {
+//                 alert('FAILED...', error);
+//             });
+//     });
+// }
